7a4eff04b76dd73e7d8a27d58c24f933f24ba0e5
kovaxarny <kovaxarny96@gmail.com>
2018-03-12 15:17:34 +0100

    JUnit dependency and a simple test added

--- src/test/java/com/epam/internship/carrental/CarrentalApplicationTests.java
+++ src/test/java/com/epam/internship/carrental/CarrentalApplicationTests.java
@@ -1,5 +1,7 @@
 package com.epam.internship.carrental;
 
+import com.epam.internship.carrental.model.Car;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,15 @@ public class CarrentalApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void CarCreation() {
+		Car car = new Car();
+		car.setId(1);
+		car.setType("Ford");
+
+		Assert.assertEquals("It's fine ", car.toString(),"Car{type='Ford'}" );
 	}
 
 }
